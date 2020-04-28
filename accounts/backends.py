d09f0dda060ea21468b35eca38fded2afebb8683
aidan-stritch <aidan.stritch@gmail.com>
2020-04-28 20:53:08 +0000

    added shopping cart functionality to site

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
-class EmailAuth: 
+class EmailAuth:
     """Authenticate a user by an exact match on the email and password"""
     def authenticate(self, username=None, password=None):
         """Get an instane of user based off the email and verify the password"""
